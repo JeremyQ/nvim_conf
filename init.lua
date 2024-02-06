1ac124a1eb105a523437cb4ce40c1c0265e35cbb
Jeremy <jeremyq95@gmail.com>
2024-02-06 10:07:33 -0800

    switching to tokyonight theme

--- init.lua
+++ init.lua
@@ -67,7 +67,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
